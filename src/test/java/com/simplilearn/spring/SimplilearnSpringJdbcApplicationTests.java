fbb082da9452fb10900bf38ea59aef71d0b2822f
cclarke74 <clairc2@gmail.com>
2024-01-14 15:27:34 -0800

    Add Spring Test for Jenkins

--- src/test/java/com/simplilearn/spring/SimplilearnSpringJdbcApplicationTests.java
+++ src/test/java/com/simplilearn/spring/SimplilearnSpringJdbcApplicationTests.java
@@ -1,13 +1,23 @@
 package com.simplilearn.spring;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.simplilearn.spring.controller.UserController;
 
 @SpringBootTest
 class SimplilearnSpringJdbcApplicationTests {
 
+	@Autowired
+	UserController userController;
+	
 	@Test
 	void contextLoads() {
+		assertThat(userController).isNotNull();
+		
 	}
 
 }
